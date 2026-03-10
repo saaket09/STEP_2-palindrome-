13395aa9b5a944ba9b0fd0a81aae913208c9e6b5
saaket09 <saaketelluru2006@gmail.com>
2026-03-10 10:08:04 +0530

    UC1: Add welcome message module

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -2,4 +2,9 @@ import java.util.Scanner;
 
 public class PalindromeChecker {
 
+  public   static void main(String[] args) {
+        System.out.println("welcome to palindrome checker");
+        System.out.println("version 1 ");
+      System.out.println("System initialized successfully");
+    }
 }
